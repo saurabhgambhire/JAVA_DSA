59464326edb904939bebcfbc19388e64198ad49c
saurabh_gam <saurabh.gambhire@leadtorev.com>
2023-09-21 09:32:10 +0530

    .

--- src/main/java/LeetCode/Q34_FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/LeetCode/Q34_FindFirstAndLastPositionOfElementInSortedArray.java
@@ -3,37 +3,10 @@ package LeetCode;
 import java.util.Arrays;
 
 public class Q34_FindFirstAndLastPositionOfElementInSortedArray {
-//    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
-
-   /*
-           *
-           *  Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
-
-        If target is not found in the array, return [-1, -1].
-
-        You must write an algorithm with O(log n) runtime complexity.
-
-
-
-        Example 1:
-
-        Input: nums = [5,7,7,8,8,10], target = 8
-        Output: [3,4]
-        Example 2:
-
-        Input: nums = [5,7,7,8,8,10], target = 6
-        Output: [-1,-1]
-        Example 3:
-
-        Input: nums = [], target = 0
-        Output: [-1,-1]
-        * */
-
+    //    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
     public static void main(String[] args) {
-
         int[] nums = {5, 7, 7, 8, 8, 10};
         int target = 8;
-
         System.out.println(Arrays.toString(searchRange(nums, target)));
     }
 
@@ -58,21 +31,18 @@ public class Q34_FindFirstAndLastPositionOfElementInSortedArray {
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-
-            if (target < nums[mid]) {
+            if (target < nums[mid])
                 end = mid - 1;
-            } else if (target > nums[mid]) {
+            else if (target > nums[mid])
                 start = mid + 1;
-            } else {
+            else {
                 ans = mid;
-                if (findStartIndex) {
+                if (findStartIndex)
                     end = mid - 1;
-                } else {
+                else
                     start = mid + 1;
-                }
             }
         }
         return ans;
     }
-
 }
