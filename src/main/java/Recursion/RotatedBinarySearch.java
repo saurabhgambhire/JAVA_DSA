8a3a7d24142f290ead0ec35e7349077b19e71e0f
Saurabh Gambhire <saurabhgambhire57@gmail.com>
2023-10-04 22:10:36 +0530

    .

---
+++ src/main/java/Recursion/RotatedBinarySearch.java
@@ -0,0 +1,30 @@
+package Recursion;
+
+public class RotatedBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {5,6,7,8,9,1,2,3};
+        System.out.println(binarySearch(arr,8, 0, arr.length-1));
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        if (start > end)
+            return -1;
+
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[start] <= arr[mid]) {
+            if (target >= arr[start] && target <= arr[mid])
+                return binarySearch(arr, target, start, mid - 1);
+            else
+                return binarySearch(arr, target, mid + 1, end);
+        }
+
+        if (target >= arr[mid] && target <= arr[end])
+            return binarySearch(arr, target, mid + 1, end);
+
+        return binarySearch(arr, target, start, mid - 1);
+    }
+}
