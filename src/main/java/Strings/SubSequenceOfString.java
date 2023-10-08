e8a0d34932f3bb935c5efdf88981740ee898bcd9
Saurabh Gambhire <saurabhgambhire57@gmail.com>
2023-10-08 09:03:05 +0530

    .

--- src/main/java/Strings/SubSequenceOfString.java
+++ src/main/java/Strings/SubSequenceOfString.java
@@ -1,8 +1,12 @@
 package Strings;
 
+import java.util.ArrayList;
+
 public class SubSequenceOfString {
     public static void main(String[] args) {
-        subSequence("", "abc");
+//        subSequence("", "abc");
+        ArrayList<String> list = subSequence1("", "abc");
+        System.out.println(list);
     }
 
     private static void subSequence(String p, String up) {
@@ -15,5 +19,19 @@ public class SubSequenceOfString {
 
         subSequence(p + ch, up.substring(1));
         subSequence(p, up.substring(1));
+    }
+
+    private static ArrayList<String> subSequence1(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> strings = new ArrayList<>();
+            strings.add(p);
+            return strings;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String> left = subSequence1(ch + p, up.substring(1));
+        ArrayList<String> right = subSequence1(p, up.substring(1));
+        left.addAll(right);
+        return left;
     }
 }
