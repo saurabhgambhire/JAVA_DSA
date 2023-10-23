751f63e772168a1bf2719945f464412fa21e54bc
Saurabh Gambhire <saurabhgambhire57@gmail.com>
2023-10-23 18:48:38 +0530

    ,

---
+++ src/main/java/LeetCode/Q141_LinkedListCycle.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class Q141_LinkedListCycle {
+    //https://leetcode.com/problems/linked-list-cycle/description/
+    public static void main(String[] args) {
+
+    }
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast !=null && fast.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow)
+                return true;
+        }
+        return false;
+    }
+}
