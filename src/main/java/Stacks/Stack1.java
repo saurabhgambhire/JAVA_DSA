9c13d6a54e377cbdd4652038a56e26a640140f42
Saurabh Gambhire <saurabhgambhire57@gmail.com>
2023-11-01 18:05:00 +0530

    .

---
+++ src/main/java/Stacks/Stack1.java
@@ -0,0 +1,16 @@
+package Stacks;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class Stack1 {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 10; i++)
+            stack.push(i);
+
+//        for (Integer integer : stack) System.out.println(integer);
+        while (!stack.isEmpty())
+            System.out.println(stack.pop());
+    }
+}
